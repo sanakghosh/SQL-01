d26d0f8eefb3dddf3de2f6f37a9284118fe8ff78
Sanak Ghosh <sanakghosh3631@gmail.com>
2024-09-06 04:17:49 +0600

    SQL Basics

---
+++ SQLQuery_1.sql
@@ -0,0 +1,41 @@
+-- CREATE TABLE EmployeeSalary(
+--     EmployeeId INT,
+--     JobTitle varchar(50),
+--     Salary INT
+-- )
+
+-- INSERT INTO EmployeeDemographics VALUES
+-- (1001, 'Jim', 'Bond', 30, 'Male'),
+-- (1002, 'Bim', 'Chy', 40, 'Male'),
+-- (1003, 'Rim', 'Benote', 36, 'Female'),
+-- (1004, 'Kim', 'Donald', 29, 'Male'),
+-- (1005, 'Sim', 'Will', 32, 'Female'),
+-- (1006, 'Dim', 'Bill', 44, 'Male'),
+-- (1007, 'Him', 'Jill', 38, 'Male')
+
+-- INSERT INTO EmployeeSalary VALUES
+-- (1001, 'HR', 50000),
+-- (1002, 'Manager', 65000),
+-- (1003, 'Marketer', 47000),
+-- (1004, 'Salesman', 49000),
+-- (1005, 'Data Analyst', 49000)
+
+-- Insert into EmployeeDemographics VALUES
+-- (1008, 'Ryan', 'Howard', 26, 'Male'),
+-- (NULL, 'Holly', 'Flax', NULL, NULL),
+-- (1010, 'Darryl', 'Philbin', NULL, 'Male')
+
+-- Create Table WareHouseEmployeeDemographics 
+-- (EmployeeID int, 
+-- FirstName varchar(50), 
+-- LastName varchar(50), 
+-- Age int, 
+-- Gender varchar(50)
+-- )
+
+-- Insert into WareHouseEmployeeDemographics VALUES
+-- (1013, 'Darryl', 'Philbin', NULL, 'Male'),
+-- (1050, 'Roy', 'Anderson', 31, 'Male'),
+-- (1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+-- (1052, 'Val', 'Johnson', 31, 'Female')
+
